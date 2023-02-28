22ca9e0620a80db4eccdde37dd5cb367a448dbb1
Dhruv Khamar <113462419+DhruvKhamar@users.noreply.github.com>
2023-02-27 22:28:30 -0500

    Uploading assignment

---
+++ MyStory/MyStoryProject/MyStoryProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyStoryProject
+//
+//  Created by Dhruv Khamar on 2/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var customlabel: UILabel!
+    
+    @IBOutlet weak var custombutton1: UIButton!
+    
+    @IBOutlet weak var custombutton2: UIButton!
+    
+    @IBOutlet weak var custombutton3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
